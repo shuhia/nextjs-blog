4d62c7a1ac3bfa4f11be607f0b9a1960b51b0016
Alex On <shuhia.on@gmail.com>
2021-07-11 04:17:56 +0200

    Moved todolist and todos to components

--- components/todolist/Todo.js
+++ components/todolist/Todo.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { CustomLink as Link } from "./CustomLink";
 
-const todo = () => {
-  return <div></div>;
+const TodoColumn = (props) => {
+  const { type, completed } = props;
+  return <td className={completed ? "completed" : ""}>{type}</td>;
 };
 
-export default todo;
+const Todo = (props) => {
+  const todo = props.todo;
+  const { todo_completed, todo_description, todo_responsible, todo_priority } =
+    todo;
+  return (
+    <tr>
+      <TodoColumn
+        type={todo_description}
+        completed={todo_completed}
+      ></TodoColumn>
+      <TodoColumn
+        type={todo_responsible}
+        completed={todo_completed}
+      ></TodoColumn>
+      <TodoColumn type={"Status"} completed={todo_completed}></TodoColumn>
+      <TodoColumn type={todo_priority} completed={todo_completed}></TodoColumn>
+      <td>
+        <Link to={"todolist/edit/" + props.todo._id}>Edit</Link>
+      </td>
+    </tr>
+  );
+};
+
+export default Todo;
