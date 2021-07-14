087177c9e147fb6d87a12233e6473741a3bf08e0
Alex On <shuhia.on@gmail.com>
2021-07-14 09:52:32 +0200

    add: LinkList and LinkItem

--- components/main/Navbar.js
+++ components/main/Navbar.js
@@ -2,9 +2,20 @@ import React, { useEffect, useState } from "react";
 import navStyles from "../../styles/Nav.module.scss";
 import Link from "next/link";
 
-const Nav = () => {
-  const [hide, setHide] = useState(false);
+const LinkList = (props) => {
+  return <ul>{props.children}</ul>;
+};
 
+const LinkItem = ({ children, setCurrentLink, ...rest }) => {
+  return (
+    <li>
+      <Link {...rest}>{children}</Link>
+    </li>
+  );
+};
+
+const Navbar = () => {
+  const [currentLink, setCurrentLink] = useState(false);
   const createLinks = function (n = 0) {
     return [...Array(n).keys()].map((e, id) => (
       <li key={"li-" + id}>
@@ -14,43 +25,26 @@ const Nav = () => {
       </li>
     ));
   };
-  const links = createLinks();
-
-  function onScroll(e) {
-    setHide(true);
-  }
-  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 
   return (
     <div className={navStyles.container}>
       <div className={navStyles.nav}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/music">music</Link>
-          </li>
-          <li>
-            <Link href="/videos">videos</Link>
-          </li>
-          <li>
-            <Link href="/todolist">todolist</Link>
-          </li>
-          <li>
-            <Link href="/slideshow">slideshow</Link>
-          </li>
-          {links}
-        </ul>
+        <LinkList>
+          <LinkItem href="/">Home</LinkItem>
+
+          <LinkItem href="/blog">Blog</LinkItem>
+
+          <LinkItem href="/about">About</LinkItem>
+
+          <LinkItem href="/videos">videos</LinkItem>
+
+          <LinkItem href="/todolist">todolist</LinkItem>
+
+          <LinkItem href="/slideshow">slideshow</LinkItem>
+        </LinkList>
       </div>
     </div>
   );
 };
 
-export default Nav;
+export default Navbar;
--- pages/music.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const music = () => {
-  return <div></div>;
-};
-
-export default music;
